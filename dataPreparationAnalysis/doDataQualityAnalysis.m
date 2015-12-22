2237bcc6251503c4b374cf42643ceab18722636e
timjp <timjp@TJP-T7610>
2015-12-22 14:20:07 +1100

    Public release of GUI plus omitted files
    Former-commit-id: 85e113a33d1bb1e2b167e6e01fd8abe63c79c0e3

---
+++ dataPreparationAnalysis/doDataQualityAnalysis.m
@@ -0,0 +1,171 @@
+function headData = doDataQualityAnalysis( headData, boreDepth, surface_elevation, casing_length, constuction_date, ...
+   checkMinSartDate, checkMaxEndDate, chechDuplicateDates, checkMinHead, checkMaxHead, RateofChangeThreshold, ConstHeadThreshold, outlierNumStDevs )
+%EXPORTDATATABLE Summary of this function goes here
+%   Detailed explanation goes here
+
+    % Minimum number of non-errorous observations required for undertaking
+    % ARAM outlier detection;
+    minObsforOutlierDetection = 12;
+
+    % Duration for constant head error checkd min ob 
+    constHeadThreshold_minObs = 4;
+    
+    % Assign plausible dates for water level obs    
+    plausibleEndDate = now();    
+    
+    errCode = -9999.99;
+    
+    % Check there is enough data to run the analysis
+    if size(headData,1)<=1
+        return;
+    end
+    
+    
+    % Sort by date 
+    headData = sortrows(headData,1);
+
+    % Convert the table data to arrays
+    if istable(headData)
+        headData = headData{:,[1:2]};
+    end
+
+    % Filter for plausible dates
+    filt_date = false(size(headData,1),1);
+    if checkMinSartDate
+        filt_date = headData(:,1) < constuction_date;
+    end
+    if checkMaxEndDate
+        filt_date = headData(:,1)>now() | filt_date;
+    end    
+    isErrorObs = filt_date;
+
+    % Filter date duplicates
+    filt_duplicates = false(size(headData,1),1);
+    if chechDuplicateDates
+        timeStep = [diff( headData(:,1)); inf];
+        filt_duplicates = timeStep==0;    
+        filt_duplicates(isErrorObs) = false;        
+    end
+    isErrorObs = filt_date | filt_duplicates;
+
+    % Check head is above the bottom of the bore.
+    filt_minHead = false(size(headData,1),1);
+    if checkMinHead                    
+        filt_minHead_tmp = headData(~isErrorObs,2) < surface_elevation - boreDepth;        
+        filt_minHead(~isErrorObs ) = filt_minHead_tmp;
+        clear filt_minHead_tmp;    
+    end
+    isErrorObs = filt_date | filt_duplicates | filt_minHead;
+
+    
+    % Check the head is below the top of the casing (assumes the aquifer is
+    % unconfined)
+    filt_maxHead = false(size(headData,1),1);
+    if checkMaxHead
+        filt_maxHead_tmp = headData(~isErrorObs,2) > surface_elevation + casing_length;        
+        filt_maxHead(~isErrorObs ) = filt_maxHead_tmp;
+        clear filt_maxHead_tmp;
+    end
+    isErrorObs = filt_date | filt_duplicates | filt_minHead | filt_maxHead;
+
+    
+    % Filter for rapd change in headData
+    filt_rapid = false(size(headData,1),1);    
+    d_headData_dt = diff( headData(~isErrorObs,2))./ diff( headData(~isErrorObs,1));
+    filt_rapid(~isErrorObs) = [abs(d_headData_dt) >= RateofChangeThreshold; false];          
+    isErrorObs = filt_date | filt_duplicates | filt_minHead | filt_maxHead | filt_rapid;
+            
+    % Filter out bore with a constant head for > 'ConstHeadThreshold' days. First the
+    % duration of 'flat' periods is assessed.
+    filt_flatExtendedDuration = false(size(isErrorObs,1),1);
+    if RateofChangeThreshold>0
+        headData_tmp = headData(~isErrorObs,:);    
+        delta_headData_fwd = [false; headData_tmp(2:end,2) - headData_tmp(1:end-1,2)];
+        delta_headData_rvs = [headData_tmp(1:end-1,2) - headData_tmp(2:end,2); false];
+        filt_flat = delta_headData_fwd==0 | delta_headData_rvs==0;
+        if any(filt_flat)
+
+            filt_flatExtendedDuration_tmp = false(sum(~isErrorObs),1);
+
+            % Filt out prior identified errors
+            headData_filt = headData(~isErrorObs,:);
+
+            startDate = 0;
+            endDate = 0;
+            startheadData = nan;
+            for j=2:size(headData_filt,1)
+                try
+                    if (filt_flat(j) && ~filt_flat(j-1)) || (j==2 && filt_flat(j))
+                        if j==2 && filt_flat(j-1)
+                            startDate = headData_filt(j-1,1)-sqrt(eps());
+                            startheadData = headData_filt(j-1,2);
+                        else
+                            startDate = headData_filt(j,1);
+                            startheadData = headData_filt(j,2);
+                        end                            
+                        endDate = 0;                            
+                    elseif startDate>0 && (~filt_flat(j) || j==size(headData_filt,1) || headData_filt(j,2)~=startheadData) 
+                        endDate = headData_filt(j,1);
+                        if j==size(headData_filt,1) && headData_filt(j,2)==headData_filt(j-1,2)
+                            endDate = endDate+sqrt(eps());
+                        end
+
+                        % Assess if the zero period is >60 days long
+                        % and are > constHeadThreshold_minObs
+                        filt_tmp = headData_filt(:,1)>= startDate & headData_filt(:,1) < endDate;
+                        consHead_dates =  headData_filt(filt_tmp,1);
+                        if  consHead_dates(end) - consHead_dates(1) >= ConstHeadThreshold ...
+                        &&  sum(filt_tmp)>=constHeadThreshold_minObs       
+                            filt_flatExtendedDuration_tmp(filt_tmp) = true;
+                        end    
+
+                        % Reset markers
+                        startDate = 0;
+                        endDate = 0;   
+                        startheadData = nan;                            
+                    end
+                catch
+                   display('errr'); 
+                end
+            end     
+            filt_flatExtendedDuration = false(size(isErrorObs,1),1);
+            filt_flatExtendedDuration(~isErrorObs) = filt_flatExtendedDuration_tmp;
+        end
+    end
+
+    % Aggregare Errors filters
+    isErrorObs = filt_date | filt_duplicates | filt_minHead | filt_maxHead | filt_rapid | filt_flatExtendedDuration;  
+
+
+    % Detect remaining outliers using a calibrated ARMA(1) model.            
+    if sum(~isErrorObs)>minObsforOutlierDetection && outlierNumStDevs>0
+        try
+            [ isOutlierObs, noise_sigma, ARMA_params, exp_model ] = outlierDetection(  headData, isErrorObs, outlierNumStDevs);
+        catch ME
+            display(['    WARNING: Outlier detection failed.']); 
+            isOutlierObs = false(size(isErrorObs));
+            noise_sigma = [];
+            ARMA_params = [];           
+            exp_model = [];
+        end
+    else
+        isOutlierObs = false(size(isErrorObs));
+        noise_sigma = [];
+        ARMA_params = [];                
+        exp_model = [];
+    end
+
+
+    % Delete calibration data files
+    delete('*.dat');
+    
+    % Aggregate the logical data from the analyis into a table and combine
+    % with the observated data.
+    headData = table(year(headData(:,1)), month(headData(:,1)), day(headData(:,1)), hour(headData(:,1)), minute(headData(:,1)), headData(:,2), filt_date, filt_duplicates, filt_minHead, filt_maxHead, filt_rapid, filt_flatExtendedDuration, isOutlierObs, ...
+        'VariableNames',{'Year', 'Month', 'Day', 'Hour', 'Minute', 'Head', 'Date_Error', 'Duplicate_Date_Error', 'Min_Head_Error','Max_Head_Error','Rate_of_Change_Error','Const_Hear_Error','Outlier_Obs'});
+
+end
+
+
+                   
+                
---
+++ dataPreparationAnalysis/outlierDetection.m
@@ -0,0 +1,135 @@
+function [ isOutlier, noise_sigma, x_opt, model_calib ] = outlierDetection( headData, isOutlier, nSigma_threshold)
+
+    % Initialise outputs    
+    noise_sigma = inf;
+    x_opt = [];
+    model_calib = [];
+     
+    % Initialise 'isOutliers' if its not supplied by the user
+    if isempty(isOutlier)
+        isOutlier = false(size(headData,1));
+    end
+    isNewOutlier = false(size(headData,1));
+    isOutlier_input = isOutlier;
+    
+    % Build inputs for exponential smoothing model
+    t = headData(:,1);
+    h_obs = headData(:,2);
+    dummyBoreID = 'BoreID_123';
+    coordinates = {dummyBoreID, -999, -999; 'Precip', -999, -999};
+    forcingData = [t(1)-10: t(end)+10]';
+    forcingData = table(year(forcingData),month(forcingData),day(forcingData),zeros(size(forcingData,1),1),'VariableNames',{'Year';'Month';'Day';'Precip'});   
+    h_obs_model = [year(t), month(t), day(t), hour(t), minute(t), second(t), h_obs];
+    
+    % Calibrate exponential smoothing model
+    noise_sigma = 0;
+    i=1;
+    doFinalCalibration = false;
+    while i==1 || sum(isNewOutlier)/size(isNewOutlier,1) > normpdf(nSigma_threshold*noise_sigma,0.0,noise_sigma) || doFinalCalibration
+
+        % Build model        
+        model_calib = GroundwaterStatisticsToolbox('Outlier detection', dummyBoreID, 'ExpSmooth', h_obs_model(~isOutlier,:), -999, forcingData, coordinates, false);
+        
+        % Calibrate model
+        calibrateModel(model_calib, 0, inf, 0)
+        
+        % Get the standard deviation of the noise.
+        noise_sigma = model_calib.model.variables.sigma_n;
+        
+        % Exit if the this loop is being undertaken 
+        if doFinalCalibration
+            break
+        end
+        
+        % Store calibrated parameters
+        alpha = model_calib.model.parameters.alpha;
+        beta = model_calib.model.parameters.beta;
+        gamma = model_calib.model.parameters.gamma;
+        meanHead_calib = model_calib.model.variables.meanHead_calib;
+        initialHead = model_calib.model.variables.initialHead;
+        initialTrend = model_calib.model.variables.initialTrend; 
+         
+        % Loop through each non-outlier observation to omit is from the
+        % simulation. THis is doen to exclude a possible outlier point from
+        % the smoothened estimate and the resulting calculation of the
+        % noise. If the difference between the current obs point and the
+        % forcast is greater than this noise estimate, then it is denoted
+        % as an outlier. Importantly, in calculating the noise the min and
+        % max points are also excluded.
+        isNewOutlier = false(size(isOutlier));        
+        filt = isOutlier;
+        ind = find(~isOutlier)';   
+        k=1;
+        for j=ind(2:end)
+            % Get a vector of obs points excluding the current obs point, point ind(j).
+            k=k+1;
+            filt(j) = true;
+            time_points_trim =  headData(~filt,1);
+            h_obs_trim = headData(~filt,2);
+            h_obs_trim = [year(time_points_trim), month(time_points_trim), day(time_points_trim), hour(time_points_trim), minute(time_points_trim), second(time_points_trim), h_obs_trim];
+
+            % Rebuild the model without the current time point, assign the
+            % calibrated parameters and solve the model.
+            model = GroundwaterStatisticsToolbox('Outlier detection', dummyBoreID, 'ExpSmooth', h_obs_trim, -999, forcingData, coordinates, false);
+            model.model.parameters.alpha = alpha;
+            model.model.parameters.beta = beta;
+            model.model.parameters.gamma = gamma;
+            model.model.variables.meanHead_calib = meanHead_calib;
+            model.model.variables.calibraion_time_points = time_points_trim;            
+            model.model.variables.initialHead = initialHead;
+            model.model.variables.initialTrend = initialTrend; 
+            
+            % Add current point back in the simulation. Note, when
+            % the simulation is undertaken for a point does not exist in
+            % model, then it is forecast.
+            filt(j) = false;
+            time_points_trimExtended =  headData(~filt,1);            
+            h_mod_trim = solveModel(model, time_points_trimExtended, [], '', 'NoLabel',false);            
+            h_forecast_trim = model.model.variables.h_forecast;            
+            
+            % Create a filter to remove the current point from the forecast
+            % and then calculate the residuals
+            obs_filt = [1:k-1,k+1:length(ind)];
+            resid_trim = h_obs_trim(:,end) - h_forecast_trim(obs_filt);
+                        
+            % To minimise the impacts of outliers as yet identified, create
+            % a filter to remove the most negative and posative values from
+            % the residuals
+            resid_filt = resid_trim> min(resid_trim) & resid_trim < max(resid_trim);
+            resid_trim = resid_trim(resid_filt);
+            time_points_trim = time_points_trim(resid_filt);
+            
+            sigma_n_trimmed = sqrt(mean(resid_trim(1:end-1).^2 ./ (1 - exp( -2 .* 10.^model.model.parameters.beta .* diff(time_points_trim) ))));        
+            
+            % Calculate residual for omitted obs point.
+            resid_point = h_obs(j) - h_forecast_trim(k);
+            
+            % Break for-loop if an outlier is detected.
+            if abs(resid_point) >= nSigma_threshold* sigma_n_trimmed;
+                isNewOutlier(j) = true;
+                break;
+            end
+        end
+        
+        % Aggregate new outliers with previously detected outliers
+        isOutlier = isOutlier | isNewOutlier;
+        
+        % If the while loop is to exit, then set flag to do one last
+        % calibration so that the noise is best estimated.
+        if sum(isNewOutlier)/size(isNewOutlier,1) < normpdf(nSigma_threshold*noise_sigma,0.0,noise_sigma)
+            doFinalCalibration = true;
+        end
+        
+        %update counter
+        i=i+1;
+        
+    end
+
+    % Assign the final parameters 
+    x_opt = getParameters(model_calib.model);
+    
+    % Exclude input outliers from those input. That is, only return the
+    % outliers identified from the exponential smoothing model
+    isOutlier(isOutlier_input) = false;
+end
+
