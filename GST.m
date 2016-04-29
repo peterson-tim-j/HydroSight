6861c165a12766054c07772c15c1e0b2879e7c2e
Tim Peterson <peterson.tim.j@gmail.com>
2016-04-29 15:13:45 +1000

    GUI start comment comments cleaned up.
    Former-commit-id: 18bc9362a658b28d2e3c7341b834faf675d1cb96

--- GST.m
+++ GST.m
@@ -7,22 +7,8 @@ function GST()
     rmpath(genpath( fullfile( pwd, '.git')));
     
     % Load GUI
-%    try
-         GST_GUI();
-%     catch errorData
-%         
-%         warndlg({'An unexpected error occurred with the user interface.','','After clicking OK, you will be asked to save an error file.','Please email the error file and your model file to timjp@unimelb.edu.au'},'Unexpected Error ...')
-%         
-%         [fName,pName] = uiputfile({'*.mat'},'Save error file as...');    
-%         if fName~=0;
-%             fname = fullfile(pName, fName);
-%             try
-%                 save(fname, 'errorData');  
-%             catch
-%                 warndlg('The error file could not be saved.','Unexpected File Save Error ...')
-%             end
-%         end
-%     end
+     GST_GUI();
+
 
 end
 
