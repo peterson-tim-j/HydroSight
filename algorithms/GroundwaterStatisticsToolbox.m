5baf5c5347abfc3cf9360f32e1339ae4f9b86883
Tim Peterson <peterson.tim.j@gmail.com>
2016-10-19 14:59:12 +1100

    Compatability added for loading models of original class 'GroundwaterStatisticalToolbox'.

---
+++ algorithms/GroundwaterStatisticsToolbox.m
@@ -0,0 +1,16 @@
+classdef GroundwaterStatisticsToolbox < HydroSightModel
+    %GROUNDWATERSTATISICALTOOLBOX Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+        function obj = GroundwaterStatisticsToolbox(model_label, bore_ID, model_class_name, obsHead, obsHead_maxObsFreq, forcingData, siteCoordinates, varargin)
+            % Call constructor for hydroSight model,
+            obj@HydroSightModel(model_label, bore_ID, model_class_name, obsHead, obsHead_maxObsFreq, forcingData, siteCoordinates, varargin)            
+        end
+    end
+    
+end
+
