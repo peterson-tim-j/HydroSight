e1e9ac4ad30b2f7cc7a8bfca8c7dd56fb043aa56
Tim Peterson <tim.peterson@monash.edu>
2022-11-26 13:43:42 +1100

    Testing split into setup and run testing
    Changes made to allow github actions run run testing enviro setup.

--- testHydroSight.m
+++ testHydroSight.m
@@ -1,35 +1,64 @@
-% Close parallel pool. This is done to ensure code within a parfor is 
-% included in code coverage report
-poolobj = gcp('nocreate');
-delete(poolobj);
+function results = testHydroSight(runTests)
 
-% Ensure parpool does not auto-start
-ps = parallel.Settings;
-ps_Auto = ps.Pool.AutoCreate;
-ps.Pool.AutoCreate = false;
+    disp('SETTING UP TEST ENVIONMENT');
+    disp('--------------------------')
 
-% Add paths to unit tests
-addpath('testing');
-currentPath = pwd();
+    disp('Import testing plugins ...')
+    import matlab.unittest.plugins.CodeCoveragePlugin
 
-% Install GUI layout if not installed
-if isempty(ver('layout'))  
-    installedToolbox = matlab.addons.toolbox.installToolbox(fullfile(currentPath,'testing','GUI Layout Toolbox 2.3.5.mltbx'),true);
+    % Close parallel pool. This is done to ensure code within a parfor is
+    % included in code coverage report
+    disp('Closing parpool...')
+    poolobj = gcp('nocreate');
+    delete(poolobj);
+    
+    % Ensure parpool does not auto-start
+    disp('Ensure parpool is not automatically started ...')
+    ps = parallel.Settings;
+    ps_Auto = ps.Pool.AutoCreate;
+    ps.Pool.AutoCreate = false;
+    
+    % Add paths to unit tests
+    disp('Adding testing to path ...')
+    addpath('testing');
+    currentPath = pwd();
+    
+    % Install GUI layout if not installed
+    if isempty(ver('layout'))
+        disp('Installng GUI Layout toolbox ...')
+        matlab.addons.toolbox.installToolbox(fullfile(currentPath,'testing','GUI Layout Toolbox 2.3.5.mltbx'),true);
+    end
+    disp('--------------------------')
+    disp(' ')
+
+    if runTests || nargin==0
+        disp('STARTING TESTS');
+        disp('--------------------------')
+               
+        % Setyp tests
+        disp('Making test suite ...')
+        suite = testsuite({'runOutlierDetection','buildExampleModels','simulateExampleModels','calibrateNewModel'});
+        %suite = testsuite({'calibrateNewModel'});        
+        runner = testrunner("textoutput");
+
+        % add coverage report
+        disp('Adding coverage report ...')
+        runner.addPlugin(CodeCoveragePlugin.forFolder({'GUI','algorithms'},'IncludeSubfolders',true));
+
+        disp('Run tests...')
+        results = runner.run(suite);
+    
+        disp('FINSIHED TESTS');
+        disp('--------------------------')
+        disp('')
+        
+        % Rest file paths.
+        disp('Restoring paths ...')
+        rmpath(fullfile(currentPath,'testing'));
+        cd(currentPath);
+    
+        % Restore parpool settings
+        disp('Restoring parpool settings ...')
+        ps.Pool.AutoCreate = ps_Auto;
+    end
 end
-
-% Run tests
-suite = testsuite({'runOutlierDetection','buildExampleModels','simulateExampleModels','calibrateNewModel'});
-%suite = testsuite({'calibrateNewModel'});
-import matlab.unittest.plugins.CodeCoveragePlugin
-runner = testrunner("textoutput");
-
-% Get coverage report
-runner.addPlugin(CodeCoveragePlugin.forFolder({'GUI','algorithms'},'IncludeSubfolders',true));
-results = runner.run(suite);
-
-% Rest file paths.
-rmpath(fullfile(currentPath,'testing'));
-cd(currentPath);
-
-% Restore parpool settings
-ps.Pool.AutoCreate = ps_Auto;
