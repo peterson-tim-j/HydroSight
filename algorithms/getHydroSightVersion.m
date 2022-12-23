58ed0f9bb56b4bdc562579d0709f984dcf8fc67f
Tim Peterson <tim.peterson@monash.edu>
2022-12-23 21:13:11 +1100

    Version number updated

--- algorithms/getHydroSightVersion.m
+++ algorithms/getHydroSightVersion.m
@@ -1,6 +1,6 @@
 function [versionNumber,versionDate] = getHydroSightVersion()
 %getVersion Get verion number of HydroSight
 
-        versionNumber = '1.4.1.1';
-        versionDate= '2 December 2022';
+        versionNumber = '1.4.1.2';
+        versionDate= '23 December 2022';
 end
