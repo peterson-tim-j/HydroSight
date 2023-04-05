52790111f6defa00034b5b29fc7024195638daa0
Tim Peterson <tim.peterson@monash.edu>
2023-04-05 12:08:56 +1000

    Version year error typo fixed

--- algorithms/getHydroSightVersion.m
+++ algorithms/getHydroSightVersion.m
@@ -2,5 +2,5 @@ function [versionNumber,versionDate] = getHydroSightVersion()
 %getVersion Get verion number of HydroSight
 
         versionNumber = '1.4.1.5';
-        versionDate= ' 4 Apr. 2022';
+        versionDate= ' 4 Apr. 2023';
 end
